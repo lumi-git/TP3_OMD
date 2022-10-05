634d5d20321d098d9c3fcc1f9c757bb37d936a77
lumi-git <74414575+lumi-git@users.noreply.github.com>
2022-10-05 09:14:11 +0200

    Adding File to the giot
    adding the first file to the git !

---
+++ src/test/java/fr/esir/omd/ci/tpmaven/FirstPDFTest.java
@@ -0,0 +1,39 @@
+package fr.esir.omd.ci.tpmaven;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+public class FirstPDFTest extends TestCase{
+
+	
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public FirstPDFTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( FirstPDFTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+    
+    
+	
+	
+}
