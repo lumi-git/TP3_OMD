c6fdbd2b5d82931e523f59aacafa9366f99eb868
Ronan Tremoureux <ronantremoureux@dn063179.etu.univ-rennes1.fr>
2022-10-06 08:53:37 +0200

    formating files with googleCheckstyle format

--- src/main/java/fr/esir/omd/ci/tpmaven/App.java
+++ src/main/java/fr/esir/omd/ci/tpmaven/App.java
@@ -2,9 +2,9 @@ package fr.esir.omd.ci.tpmaven;
 
 /**
  * Hello world!.
- *
  */
 public class App {
+
   public static void main(String[] args) {
     System.out.println("Hello World!");
   }
--- src/test/java/fr/esir/omd/ci/tpmaven/FirstPDFTest.java
+++ src/test/java/fr/esir/omd/ci/tpmaven/FirstPDFTest.java
@@ -4,36 +4,31 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
-public class FirstPDFTest extends TestCase{
+public class FirstPDFTest extends TestCase {
 
-	
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public FirstPDFTest( String testName )
-    {
-        super( testName );
-    }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( FirstPDFTest.class );
-    }
+  /**
+   * Create the test case
+   *
+   * @param testName name of the test case
+   */
+  public FirstPDFTest(String testName) {
+    super(testName);
+  }
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
-    
-    
-	
-	
+  /**
+   * @return the suite of tests being tested
+   */
+  public static Test suite() {
+    return new TestSuite(FirstPDFTest.class);
+  }
+
+  /**
+   * Rigourous Test :-)
+   */
+  public void testApp() {
+    assertTrue(true);
+  }
+
+
 }
